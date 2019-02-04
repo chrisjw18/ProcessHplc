44966227406eefb33cb339c5ddc8b968c2997a8e
Christopher Williamson <cw16541@it031322.users.bris.ac.uk>
2019-02-04 10:42:39 +0000

    update to library input into package

--- data-raw/package_data_input.R
+++ data-raw/package_data_input.R
@@ -17,7 +17,15 @@ devtools::use_data(mobile_phase_reference_example, overwrite = T)
 
 #pigment library
 library <- src_sqlite('data-raw/library', create=F)
+lib.meta <- as.data.frame(tbl(library, "library_meta_data"))
+lib.abs.spec<-as.data.frame(tbl(library, "library_abs_data"))
+lib.abs.spec<-subset(lib.abs.spec, wave > 349)
+lib.vite.spec<-as.data.frame(tbl(library, "vite_abs_data"))
 devtools::use_data(library, overwrite = T)
+devtools::use_data(lib.meta, overwrite = T)
+devtools::use_data(lib.abs.spec, overwrite = T)
+devtools::use_data(lib.vite.spec, overwrite = T)
+
 
 #pigment reference retention time lines...
 ref <- read.csv('data-raw/pigment_retention_times.csv')
